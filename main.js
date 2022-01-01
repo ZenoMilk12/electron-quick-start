fa2d6cba2f0fbfb935e11d7610639813543811bf
ZenoMilk12 <xalexclashroyale@gmail.com>
2022-01-01 15:27:56 +0200

    prima schimbarea

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://vbcusefu.netlify.app')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
